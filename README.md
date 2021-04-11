9092261410260b02a1c577f72b8440e1d479119d
Wink Saville <wink@saville.com>
2021-04-11 16:31:50 -0700

    Add Symbol
    This represents a "complex" example with persistenting a struct Symbol
    with a Vec(SymbolFilters) which is a variant enum!

--- README.md
+++ README.md
@@ -1,7 +1,34 @@
 # Experiment with sled
 
 Initially this is from [sled.rs/introduction](https://sled.rs/introduction.html)
+I then used [this stackoverflow](https://stackoverflow.com/questions/58358179/using-sled-how-do-i-serialize-and-deserialize)
+as a more complex example. You can change the two if statments
+in main to not do an insert, shows persistence. And the conditional
+remove to empty the database. Also, if you delete my_storage_directory
+it will be re-created on the next run.
 
+```
+wink@3900x:~/prgs/rust/projects/expr-sled-intro (main)
+$ cargo run
+   Compiling expr-sled-intro v0.1.0 (/home/wink/prgs/rust/projects/expr-sled-intro)
+    Finished dev [unoptimized + debuginfo] target(s) in 0.60s
+     Running `target/debug/expr-sled-intro`
+y=Symbol {
+    symbol: "BNB",
+    filters: [
+        LotSize {
+            min_qty: 1.0,
+            max_qty: 100.0,
+            step_size: 1.0,
+        },
+        MarketLotSize {
+            min_qty: 20.0,
+            max_qty: 200.0,
+            step_size: 2.0,
+        },
+    ],
+}
+```
 ## License
 
 Licensed under either of
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,62 @@
+// Example code to use bincode as the serde:
+//  https://stackoverflow.com/questions/58358179/using-sled-how-do-i-serialize-and-deserialize
+
+use serde::{Deserialize, Serialize};
+use bincode;
+
+#[derive(Debug, Deserialize, Serialize)]
+pub enum SymbolFilters {
+    LotSize {
+        min_qty: f64,
+        max_qty: f64,
+        step_size: f64,
+    },
+
+    MarketLotSize {
+        min_qty: f64,
+        max_qty: f64,
+        step_size: f64,
+    },
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Symbol {
+    pub symbol: String,
+    pub filters: Vec<SymbolFilters>,
+}
+
+impl Symbol {
+    pub fn new(symbol: String, filters: Vec<SymbolFilters>) -> Self {
+        Symbol { symbol, filters, }
+    }
+}
 fn main() -> sled::Result<()> {
+
     // this directory will be created if it does not exist
     let path = "my_storage_directory";
 
-    // works like std::fs::open
+    // Open the data base and create if it doesn't exist
     let db = sled::open(path)?;
 
-    // key and value types can be `Vec<u8>`, `[u8]`, or `str`.
-    let key = "my key";
+    // Insert an element, setting to false you can test persistence.
+    if true {
+        let mut vsf = Vec::<SymbolFilters>::new();
+        vsf.push(SymbolFilters::LotSize { min_qty: 1.0, max_qty: 100.0, step_size: 1.0 });
+        vsf.push(SymbolFilters::MarketLotSize { min_qty: 20.0, max_qty: 200.0, step_size: 2.0 });
+        let sym1 = Symbol::new("BNB".to_string(), vsf);
+        db.insert(&sym1.symbol, bincode::serialize(&sym1).unwrap()).unwrap();
+    }
 
-    // `generate_id`
-    let value = db.generate_id()?.to_be_bytes();
+    // Retrieve a "symbol" BNB
+    let x = db.get("BNB")?.unwrap();
+    let y: Symbol = bincode::deserialize(&x).unwrap();
+    println!("y={:#?}", y);
 
-    dbg!(
-        db.insert(key, &value)?, // as in BTreeMap::insert
-        db.get(key)?,            // as in BTreeMap::get
-        db.remove(key)?,         // as in BTreeMap::remove
-    );
+    // Remove BNB which will currently leave DB empty
+    // in this simple one element case.
+    if false {
+        db.remove("BNB")?;
+    }
 
     Ok(())
 }
